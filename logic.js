7e37ef7ed6e5836368753379acab11be4d48f415
Prabhat Kasera <prabhatkasera@Prabhats-MacBook-Pro.local>
2021-06-17 01:09:05 +0530

    commit 2

--- logic.js
+++ logic.js
@@ -3,12 +3,18 @@ function goto(url, fallback) {
     var script = document.createElement('script'); 
 
     script.onload = function() { 
+        alert(url);
         document.location = url;
     } 
     script.onerror = function() { 
+        alert(fallback);
         document.location = fallback;
     } 
     script.setAttribute('src', url); 
 
     document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+function killme() {
+    alert("sample alert");
 }
